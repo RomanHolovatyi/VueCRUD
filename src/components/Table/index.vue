c180dc269bc0f6e54d09f2a55586f6e11779dea2
RomanHolovatyi <roma.holovatyi@gmail.com>
2018-08-07 23:34:31 +0300

    Add layout to the TableItem component

--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -1,24 +1,50 @@
 <template>
-  <div>
-    <v-btn>fwefwef</v-btn>
+  <div class="mt-3">
     <v-data-table
       :headers="headers"
       :items="tableItems"
+      disable-initial-sort
       hide-actions
       class="elevation-1"
     >
       <template slot="items" slot-scope="props">
-        <td>{{ props.item.id }}</td>
-        <td class="text-xs-right">{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.description }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
+        <td class="text-xs-left">{{ props.item.id }}</td>
+        <td class="text-xs-center">{{ props.item.name }}</td>
+        <td class="text-xs-center">{{ props.item.description }}</td>
+        <td class="text-xs-center">
+          <v-btn
+            color="blue"
+            fab
+            flat
+            slot="activator"
+            @click="openEditAdminModal(props.item)"
+          >
+            <v-icon>edit</v-icon>
+          </v-btn>
+          <v-btn
+            color="red"
+            fab
+            flat
+            slot="activator"
+            @click="openEditAdminModal(props.item)"
+          >
+            <v-icon>delete</v-icon>
+          </v-btn>
+        </td>
       </template>
       <template slot="no-data">
-        <v-alert :value="true" color="error" icon="warning">
+        <v-alert
+          :value="true"
+          color="red dark"
+          icon="warning"
+        >
           Sorry, nothing to display here :(
         </v-alert>
       </template>
     </v-data-table>
+    <div class="text-xs-center pt-2">
+      <v-btn color="primary" @click="createNewItem">Create New Item</v-btn>
+    </div>
   </div>
 </template>
 
@@ -34,11 +60,17 @@ export default {
           sortable: false,
           value: 'name'
         },
-        { text: 'Name', value: 'name', sortable: true },
-        { text: 'Description', value: 'description', sortable: true },
-        { text: 'Actions', value: 'actions' }
+        { text: 'Name', value: 'name', align: 'center', sortable: true },
+        { text: 'Description', value: 'description', align: 'center', sortable: true },
+        { text: 'Actions', align: 'center', value: 'actions', sortable: false }
       ],
-      tableItems: []
+      tableItems: [
+        {
+          id: 1,
+          name: 'name123',
+          description: 'wq qur weufewu wefwf'
+        }
+      ]
     }
   }
 }
---
+++ src/components/TableItem/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-layout fluid>
+    <v-flex fluid>
+      <v-card fluid>
+        <v-card-title>
+          <span class="headline">{{ getActionType }} news</span>
+        </v-card-title>
+        <v-card-text>
+          <v-flex xs12>
+            <v-text-field
+              v-model="formData.name"
+              label="Name"
+              @blur="$v.formData.name.$touch()"
+            />
+          </v-flex>
+          <v-flex xs12>
+            <v-text-area
+              v-model="formData.description"
+              label="Description"
+              @blur="$v.formData.description.$touch()"
+            />
+          </v-flex>
+          <v-card-actions>
+            <v-spacer/>
+            <v-btn color="red darken-1" flat @click.stop="cancel">Cancel</v-btn>
+            <v-btn color="blue darken-1" flat @click.stop="save">Save</v-btn>
+          </v-card-actions>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'TableItem',
+  data () {
+    return {
+      formData: {
+        name: '',
+        description: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
